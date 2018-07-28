2ebca44796f87f648ec1cbb66a7ed091729e326e
Kadzuya Okamoto <arow.okamoto@gmail.com>
2018-07-28 21:17:56 +0900

    Remove sample code for Reference.List.unwrap

--- src/Reference/List.elm
+++ src/Reference/List.elm
@@ -15,36 +15,7 @@ import Reference exposing (..)
 {-| Map and unwrap to list.
 This is especially useful for updating list on View of THE.
 
-    type alias Model =
-        { numbers : List Int
-        }
-
-    type Msg
-        = Inc (Reference Int (List Int))
-
-    update : Model -> Msg -> ( Model, Cmd Msg )
-    update msg =
-        case msg of
-            Inc ref ->
-                { model
-                    | numbers =
-                        root <| modify (\n -> n + 1) ref
-                }
-
-    view : Model -> Html Msg
-    view model =
-        div [] <|
-            Reference.List.unwrap
-                renderRow
-                (Reference.top model.numbers)
-
-    renderRow : Reference Int (List Int) -> Html Msg
-    renderRow ref =
-        div
-            [ Events.onClick <| Inc ref
-            ]
-            [ text <| toString <| Reference.this ref
-            ]
+See more about README.md.
 
 -}
 unwrap : (Reference a x -> b) -> Reference (List a) x -> List b
