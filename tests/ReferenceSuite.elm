f4caabba2f89744614fa87f47869a14380575728
Kadzuya Okamoto <arow.okamoto@gmail.com>
2018-07-29 03:41:25 +0900

    Add another fuzzy test

--- tests/ReferenceSuite.elm
+++ tests/ReferenceSuite.elm
@@ -9,11 +9,27 @@ import ReferenceSuite.Util exposing (reference)
 
 suite : Test
 suite =
-    describe "rootWith"
-        [ describe "RootWith"
-            [ fuzz2 reference string "is equals" <|
-                \ref a ->
-                    rootWith ref a
-                        |> Expect.equal (root <| modify (\_ -> a) ref)
+    describe "Reference"
+        [ describe "rootWith"
+            [ describe "RootWith"
+                [ fuzz2 reference string "is equals" <|
+                    \ref a ->
+                        rootWith ref a
+                            |> Expect.equal (root <| modify (\_ -> a) ref)
+                ]
+            ]
+        , describe "top"
+            [ describe "`map f (top a)`"
+                [ fuzz string "is equals to `fromRecord { this = a, rootWith = f }`" <|
+                    \str ->
+                        map String.reverse (top str)
+                            |> sameReference (fromRecord { this = str, rootWith = String.reverse })
+                ]
             ]
         ]
+
+
+sameReference : Reference a b -> Reference a b -> Expect.Expectation
+sameReference ref1 ref2 =
+    ( this ref1, root ref1 )
+        |> Expect.equal ( this ref2, root ref2 )
